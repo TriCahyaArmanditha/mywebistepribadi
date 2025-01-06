1c5011b7f7f74760f91d374ff0eb90c8aa57a068
TriCahyaArmanditha <tricahyaarmanditha.student@unibamadura.ac.id>
2025-01-06 16:07:29 +0700

    first commit

---
+++ JS/personalwebsite.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Website Design & Development", "Creative Branding Solutions", "Tailored Digital Strategies"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
